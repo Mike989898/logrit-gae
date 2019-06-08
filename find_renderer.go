f915025aec0efc5ab75f981d02caca28fad2eb37
Charles Hathaway <chathaway@logrit.com>
2019-06-08 17:08:36 -0400

    pull in repo, hand scss and Template files

---
+++ find_renderer.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/labstack/echo"
+	"github.com/wellington/go-libsass"
+	"net/http"
+	"path"
+	"log"
+	"os"
+)
+
+func make_error(err error) (func (echo.Context) error) {
+	return func(c echo.Context) error {
+		return c.String(http.StatusBadRequest, fmt.Sprintf("%v", err))
+	}
+}
+
+type TemplateRenderer struct {
+	Template string
+}
+
+type config struct {
+	Render string
+	Template TemplateRenderer
+}
+
+func find_renderer(dir string) (func(echo.Context) error) {
+	var conf config
+	fname := path.Join(dir, ".template")
+	ops, err := toml.DecodeFile(fname, &conf)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return make_error(err);
+	}
+	if !ops.IsDefined("render") {
+		return make_error(fmt.Errorf("Missing render field in %s", fname))
+	}
+	ret := make_error(fmt.Errorf("Renderer not found: %v", conf.Render))
+	switch(conf.Render) {
+	case "sass":
+		ret = func(c echo.Context) error {
+			f, err := os.Open(path.Join(dir, "index.scss"))
+			if err != nil {
+				return err
+			}
+			buf := bufio.NewReader(f)
+			var b bytes.Buffer
+			out_buf := bufio.NewWriter(&b)
+			comp, err := libsass.New(out_buf, buf)
+			if err != nil {
+				return err
+			}
+			if err := comp.Run(); err != nil {
+				log.Fatal(err)
+			}
+			out_buf.Flush()
+			return c.String(http.StatusOK, b.String())
+		}
+	case "Template":
+		ret = func(c echo.Context) error {
+			template := "home.html"
+			if ops.IsDefined("Template", "Template") {
+				template = conf.Template.Template
+			}
+			return c.Render(http.StatusOK, template, "")
+		}
+	}
+	return ret
+}
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"html/template"
 	"io"
-	"net/http"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"path/filepath"
+	"os"
+	"gopkg.in/src-d/go-git.v4"
+	"strings"
 )
+
+const repo_to_watch string = "../logrit-gae-sample/"
 
 type Template struct {
     templates *template.Template
@@ -17,16 +23,37 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 
 func main() {
 	e := echo.New()
-	e.Static("/static", "static")
+
+	_, err := git.PlainClone("sample/", false, &git.CloneOptions{
+		URL: repo_to_watch,
+		Progress: os.Stdout,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	root := "sample/"
+	// Setup templates
 	t := &Template{
-		templates: template.Must(template.ParseGlob("public/views/*.html")),
+		templates: template.Must(template.ParseGlob(root + "_templates/*.html")),
 	}
 	e.Renderer = t
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+
+	// Load folders, except for /_templates and /.git
+	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
+		if info.IsDir() && !strings.HasPrefix(path, root + ".git") &&
+			!strings.HasPrefix(path, root + "_templates") {
+			e.GET(path[len(root):] + "/", find_renderer(path))
+		}
+		return nil
 	})
-	e.GET("/hello", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "hello", "World")
-	})
+	if err != nil {
+		panic(err)
+	}
+
+	e.Use(middleware.Logger())
 	e.Logger.Fatal(e.Start(":1323"))
 }
